52620b92811d8add2a7a9d9f241f96a8baa5d443
Anna Clayton-Wright <aclaytonwright@hotmail.co.uk>
2021-10-28 19:52:00 +0100

    level 200

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //You can edit ALL of the code here
 function setup() {
     const allEpisodes = getAllEpisodes();
+    searchInputCreate(allEpisodes);
     makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
 
     const rootElem = document.getElementById("root");
-
+    rootElem.innerHTML = "";
     const div = document.createElement("div");
 
     episodeList.forEach(episode => {
@@ -31,6 +32,48 @@ function makePageForEpisodes(episodeList) {
     });
     rootElem.appendChild(div);
 }
+
+function searchInputCreate(allEpisodes) {
+    const div = document.createElement("div");
+    div.classList.add("input-container");
+    const para = document.createElement("p");
+    para.setAttribute("id", "display-episode");
+
+    const node = document.createTextNode(`Displaying ${allEpisodes.length}episodes`);
+
+    para.appendChild(node);
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "search-episode");
+    input.setAttribute("onkeyup", "searchInput()");
+
+    div.appendChild(input);
+    div.appendChild(para);
+
+    document.getElementsByTagName("body")[0].prepend(div);
+}
+
+function searchInput() {
+    const para = document.querySelector("#display-episode");
+    const input = document.getElementById("search-episode");
+    const filter = input.value.toUpperCase();
+    const filteredEpisodes = [];
+    getAllEpisodes().map((episode) => {
+        if (episode.name.toUpperCase().indexOf(filter) > -1) {
+
+            filteredEpisodes.push(episode);
+
+
+        };
+        para.textContent = `Displaying ${filteredEpisodes.length}/${getAllEpisodes().length} episodes`;
+
+        makePageForEpisodes(filteredEpisodes);
+
+
+    });
+}
+
+
 
 
 
